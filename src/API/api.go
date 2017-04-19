ebb22b33ee8f5b7de667209cfb9bf4993594f10c
samling <samlingx@gmail.com>
2017-04-18 22:38:57 -0700

    More (unnecessary) functionality

--- src/API/api.go
+++ src/API/api.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	_ "database/sql"
+	"encoding/json"
 	"fmt"
-	"html"
-	"io/ioutil"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"github.com/jmoiron/sqlx"
+	_ "html"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
+	"os"
 )
+
+type Data struct {
+	Id         string
+	SampleData []byte
+}
+
+type Sample struct {
+	Timestamp int
+	Value     float64
+}
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
@@ -16,14 +29,30 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func Get(w http.ResponseWriter, r *http.Request) {
-	resp, err := http.Get("http://sboynton.com:3000/api/Samples")
+// Formulate a connection string from environment variables
+func GetConnectionString(dbHost string, dbUser string, dbPass string, dbName string) string {
+	return fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", dbUser, dbPass, dbHost, dbName)
+}
 
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		defer resp.Body.Close()
-		body, _ := ioutil.ReadAll(resp.Body)
-		fmt.Fprintf(w, string(body))
+func Get(w http.ResponseWriter, r *http.Request) {
+	connString := GetConnectionString(os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_NAME"))
+	db := sqlx.MustConnect("mysql", connString)
+
+	db.MapperFunc(func(s string) string { return s })
+
+	rows, err := db.Queryx("SELECT Id, SampleData FROM Sample")
+
+	for rows.Next() {
+		var data Data
+		var sample Sample
+
+		err = rows.StructScan(&data)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = json.Unmarshal(data.SampleData, &sample)
+
+		fmt.Fprintf(w, "%+v", data)
 	}
 }
