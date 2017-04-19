6c96545ed8c0f134865de5080f9de77f134f3c9a
samling <samlingx@gmail.com>
2017-04-19 02:05:26 -0700

    Added basic container spawning function, got some semblance of an end-to-end workflow going

--- src/API/api.go
+++ src/API/api.go
@@ -1,58 +1,108 @@
 package main
 
 import (
-	_ "database/sql"
+	"context"
 	"encoding/json"
 	"fmt"
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
 	"github.com/gorilla/mux"
-	"github.com/jmoiron/sqlx"
-	_ "html"
+	_ "golang.org/x/net/context"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 )
 
 type Data struct {
-	Id         string
-	SampleData []byte
-}
-
-type Sample struct {
-	Timestamp int
-	Value     float64
+	Exists bool
 }
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/api/Samples", Get)
+	router.HandleFunc("/api/Samples/{Id}/VerifyAndSpawn", VerifyAndSpawn)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-// Formulate a connection string from environment variables
-func GetConnectionString(dbHost string, dbUser string, dbPass string, dbName string) string {
-	return fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", dbUser, dbPass, dbHost, dbName)
+func VerifyAndSpawn(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	// TODO: Sanitize the crap out of this (either here and/or in the extension itself):
+	id := vars["Id"]
+
+	exists := Exists(id)
+
+	if exists == true {
+		fmt.Fprintf(w, "Value exists\n")
+	} else {
+		fmt.Fprintf(w, "Value does not exist\n")
+	}
+
+	err := SpawnContainer(id)
+
+	if err != nil {
+		fmt.Fprintf(w, "Container did not spawn\n", err)
+	} else {
+		fmt.Fprintf(w, "Container spawned\n")
+	}
+
 }
 
-func Get(w http.ResponseWriter, r *http.Request) {
-	connString := GetConnectionString(os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_NAME"))
-	db := sqlx.MustConnect("mysql", connString)
-
-	db.MapperFunc(func(s string) string { return s })
-
-	rows, err := db.Queryx("SELECT Id, SampleData FROM Sample")
-
-	for rows.Next() {
-		var data Data
-		var sample Sample
-
-		err = rows.StructScan(&data)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		err = json.Unmarshal(data.SampleData, &sample)
-
-		fmt.Fprintf(w, "%+v", data)
+func Exists(id string) bool {
+	// Check if the entry exists already
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", "http://sboynton.com:3000/api/Samples/"+id+"/exists", nil)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Error when sending request ", err)
 	}
+	defer resp.Body.Close()
+
+	// Read the response body into a byte array
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	// Create a new Data struct to read into
+	var data Data
+
+	// Unmarshal our JSON byte array into a struct
+	err = json.Unmarshal(body, &data)
+
+	return data.Exists
+}
+
+func SpawnContainer(Id string) error {
+	ctx := context.Background()
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		return err
+	}
+
+	env := make([]string, 1)
+	env[0] = "YTID=" + Id
+
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: "yts",
+		Env:   env,
+	}, nil, nil, "")
+	if err != nil {
+		return err
+	}
+
+	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		return err
+	}
+
+	if _, err = cli.ContainerWait(ctx, resp.ID); err != nil {
+		return err
+	}
+
+	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+	if err != nil {
+		return err
+	}
+
+	io.Copy(os.Stdout, out)
+
+	return nil
 }
